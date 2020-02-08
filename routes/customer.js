2b8096c73a4381e7f42cb65a372380d07681358f
Loganarmack <loganarmack@gmail.com>
2020-02-08 02:23:31 -0500

    Got post request working

--- routes/customer.js
+++ routes/customer.js
@@ -1,16 +1,19 @@
 var express = require('express');
 const mdh = require('../util/mongodb')
+const bodyParser = require('body-parser');
 
 var router = express.Router();
 mdh.mongoDbHelper(postUser)
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.use(bodyParser.urlencoded({ extended: true }));
+
+/* POST new customer */
+router.post('/register', function(req, res, next) {
+  res.status(200).send(req.body);
+  console.log("Works!");
 });
 
 module.exports = router;
-
 /* dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
