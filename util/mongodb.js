db34accadc44b367ef55043e544e4f04ea8470a9
Loganarmack <loganarmack@gmail.com>
2020-02-08 18:13:43 -0500

    lol

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,9 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
-/* const url = "mongodb://heroku_b77vk55s:h8pdmt6b2hdr86s0jk111bks91@ds061691.mlab.com:61691/heroku_b77vk55s";
-global.NAME = 'heroku_b77vk55s'; */
+const url = "mongodb://heroku_b77vk55s:h8pdmt6b2hdr86s0jk111bks91@ds061691.mlab.com:61691/heroku_b77vk55s";
+global.NAME = 'heroku_b77vk55s';
 
-const url = "mongodb://localhost:27017/booking"
-global.NAME = 'booking';
+/* const url = "mongodb://localhost:27017/booking"
+global.NAME = 'booking'; */
 
 function mongoDbHelper(callback) {
 
