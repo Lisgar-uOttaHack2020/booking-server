2d421e716fe8d4d939bff2346dd1c0262ae8d883
Loganarmack <loganarmack@gmail.com>
2020-02-08 10:11:03 -0500

    removed useless files

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/customers', customerRouter);
 app.use('/consultants', consultantRouter);
-app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
