ddf6601dba8a0269f8773a4d9b2d68435a32262e
Loganarmack <loganarmack@gmail.com>
2020-02-08 03:02:00 -0500

    Added basic registration

--- routes/customer.js
+++ routes/customer.js
@@ -3,14 +3,31 @@ const mdh = require('../util/mongodb')
 const bodyParser = require('body-parser');
 
 var router = express.Router();
-mdh.mongoDbHelper(postUser)
+
+var customer = null;
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
 /* POST new customer */
-router.post('/register', function(req, res, next) {
-  res.status(200).send(req.body);
-  console.log("Works!");
+router.post('/register', function(req, res) {
+  if (!'name' in req.body) {
+    res.status(400).send('Name of customer must be defined.');
+  }
+  else if (!'email' in req.body) {
+    res.status(400).send('Email of customer must be defined.');
+  }
+  else if (!'children' in req.body) {
+      res.status(400).send('At least one child must be defined.')
+  }
+  else {
+    customer = {
+        name: req.body.name,
+        email: req.body.email,
+        children: req.body.children
+    }
+    mdh.mongoDbHelper(addCustomer);
+    res.status(200).send(req.body);
+  }
 });
 
 module.exports = router;
@@ -20,9 +37,15 @@ module.exports = router;
     db.close();
 });
  */
-function postUser(database) {
+function addCustomer(database) {
     var db = database; 
     var dbo = db.db("booking");
 
-    console.log("it worked!")
+    console.log(customer);
+
+    dbo.collection('customers').insertOne(customer, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    })
 }
