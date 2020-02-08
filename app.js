f346ebf71bd624db91e849c7f7ce47a2f50acc39
Loganarmack <loganarmack@gmail.com>
2020-02-08 08:21:38 -0500

    Updated CORS

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res) {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept');
+  });
 
 app.use('/', indexRouter);
 app.use('/customers', customerRouter);
