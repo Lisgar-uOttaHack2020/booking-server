ef874d1fa3d345307f2eca9f06c1cdc5bbe3e926
Loganarmack <loganarmack@gmail.com>
2020-02-08 23:38:07 -0500

    lol

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,9 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
-/* const url = "mongodb://heroku_b77vk55s:h8pdmt6b2hdr86s0jk111bks91@ds061691.mlab.com:61691/heroku_b77vk55s";
-global.NAME = 'heroku_b77vk55s'; */
 
-const url = "mongodb://localhost:27017/booking"
-global.NAME = 'booking';
+//IMPORTANT: SWITCH BETWEEN URLS WHEN PUSHING OR DATA WILL NOT BE STORED
+const url = "mongodb://heroku_b77vk55s:h8pdmt6b2hdr86s0jk111bks91@ds061691.mlab.com:61691/heroku_b77vk55s";
+global.NAME = 'heroku_b77vk55s';
+
+/* const url = "mongodb://localhost:27017/booking"
+global.NAME = 'booking'; */
 
 function mongoDbHelper(callback) {
 
